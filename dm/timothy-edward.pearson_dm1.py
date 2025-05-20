eb389174c6645ca958f339ba900b62b522d16796
tim-pearson <timothy-edward.pearson@epita.fr>
2025-05-20 19:27:14 +0200

    final version

--- dm/timothy-edward.pearson_dm1.py
+++ dm/timothy-edward.pearson_dm1.py
@@ -7,23 +7,22 @@ from mpqp.measures import BasisMeasure
 from mpqp.noise import Depolarizing
 from mpqp.execution import run
 from mpqp.execution.devices import AWSDevice
-from mpqp.measures import X as Pauli_X,  Y as Pauli_Y, Z as Pauli_Z
+from mpqp.measures import X as Pauli_X, Y as Pauli_Y, Z as Pauli_Z
+
 # %% Ex1
-p1 = 2 *Pauli_X @Pauli_X - Pauli_Y @ Pauli_Y + Pauli_Z @ Pauli_X
-p2 = 2 * p1 +Pauli_Y @ Pauli_X - 4 * Pauli_X @ Pauli_X
+p1 = 2 * Pauli_X @ Pauli_X - Pauli_Y @ Pauli_Y + Pauli_Z @ Pauli_X
+p2 = 2 * p1 + Pauli_Y @ Pauli_X - 4 * Pauli_X @ Pauli_X
 print((p2).to_matrix())
 mat = [[4, 2, 3, 8], [2, -3, 1, 0], [3, 1, -1, 5], [8, 0, 5, 2]]
-obs =Observable(np.array(mat))
+obs = Observable(np.array(mat))
 print(obs.pauli_string)
 
 # EX2
 # %% q1
-circ = QCircuit(6)
-targets = range(3)
-ancillas = range(3,6)
-for i in range(3):
-    circ.add(CNOT(targets[i], ancillas[i]))
-print(circ)
+circuit = QCircuit(
+    [H(0), CNOT(0, 1), BasisMeasure(shots=1000), Depolarizing(0.05)],
+)
+
 # %% q2
 device = AWSDevice.BRAKET_LOCAL_SIMULATOR
 result_global = run(circuit, device)
@@ -31,7 +30,7 @@ print("after global noise (p=0.05):")
 print(result_global)
 # %% q3
 circuit.add(Depolarizing(0.47, [0]))
-# %% q4 
+# %% q4
 circuit.add(Depolarizing(0.33, [0, 1], dimension=2, gates=[CNOT]))
 # %% q5
 
@@ -48,7 +47,7 @@ print(braket_circuit)
 # %%
 circ = QCircuit(6)
 targets = range(3)
-ancillas = range(3,6)
+ancillas = range(3, 6)
 for i in range(3):
     circ.add(CNOT(targets[i], ancillas[i]))
 print(circ)
